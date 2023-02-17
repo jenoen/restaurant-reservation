349f8ad03bf05a7db0c0431e6c1fce19a5da27dc
Jen <jen.oen889@gmail.com>
2023-02-17 10:34:13 -0800

    7.3-search results frontend

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -17,15 +17,39 @@ export default function Search() {
   function handleChange({ target }) {
     setMobileNumber(target.value);
   }
-
   function handleSubmit(event) {
     event.preventDefault();
 
-    // we will be adding our api call here
+    const abortController = new AbortController();
+
+    setError(null);
+
+    // our search query is mobile_number (the name of the column in the reservations table)
+    // the search value is our mobileNumber state
+    listReservations({ mobile_number: mobileNumber }, abortController.signal)
+      .then(setReservations)
+      .catch(setError);
+
+    return () => abortController.abort();
   }
+
+  const searchResultsBodyDisplay = () => {
+    return reservations.length > 0 ? (
+      reservations.map((reservation) => (
+        <ReservationRow
+          key={reservation.reservation_id}
+          reservation={reservation}
+        />
+      ))
+    ) : (
+      <p>No reservations found</p>
+    );
+  };
+
   return (
     <div>
       <form>
+        <ErrorAlert error={error} />
         <label htmlFor="mobile_number">Enter a customer's phone number:</label>
         <input
           name="mobile_number"
@@ -40,6 +64,22 @@ export default function Search() {
           Find
         </button>
       </form>
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Mobile Number</th>
+            <th scope="col">Time</th>
+            <th scope="col">People</th>
+            <th scope="col">Status</th>
+            <th scope="col">Seat</th>
+          </tr>
+        </thead>
+
+        <tbody>{searchResultsBodyDisplay()}</tbody>
+      </table>
     </div>
   );
 }
