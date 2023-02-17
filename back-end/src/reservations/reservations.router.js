27c4c66fdc66f81229bce2b20afc67c34cbc614b
Jen <jen.oen889@gmail.com>
2023-02-17 13:36:37 -0800

    created methodNotAllowed and asyncErrorBoundary; edited routers.js and controller.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,20 +6,12 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("./errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
-
-// router.route("/new").post(controller.list);
-
-// router
-//   .route("/:movieId/reviews")
-//   .get(controller.readReviewsByMovieId)
-//   .all(methodNotAllowed);
-
-// router.route("/:movieId").get(controller.read).all(methodNotAllowed);
-
-// router.route("/?is_showing=true").get(controller.list).all(methodNotAllowed);
-
-// router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
