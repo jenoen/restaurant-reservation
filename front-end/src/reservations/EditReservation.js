e32c903e30459bbd20a559fa3a9277ec878d2e02
Jen <jen.oen889@gmail.com>
2023-03-06 14:17:03 -0800

    consolidated reserveForm

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import ErrorAlert from "../layout/ErrorAlert";
 import { editReservation, findReservation } from "../utils/api";
+import ReserveForm from "./ReserveForm";
 
 function EditReservation() {
   const history = useHistory();
@@ -9,184 +10,190 @@ function EditReservation() {
   const [formData, setFormData] = useState();
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    async function loadEditReservation() {
-      try {
-        setError(null);
-        const response = await findReservation(
-          reservation_id,
-          abortController.signal
-        );
-        const thisDate = response.reservation_date.split("T")[0];
-        setFormData({
-          ...response,
-          reservation_date: thisDate,
-        });
-      } catch (error) {
-        setError(error);
-        console.error(error);
-      }
-    }
-    loadEditReservation();
-    return () => {
-      abortController.abort();
-    };
-  }, [reservation_id]);
+  // // this loads the data for the specific reservation onto the form
+  // useEffect(() => {
+  //   const abortController = new AbortController();
+  //   async function loadEditReservation() {
+  //     try {
+  //       setError(null);
+  //       const response = await findReservation(
+  //         reservation_id,
+  //         abortController.signal
+  //       );
+  //       const thisDate = response.reservation_date.split("T")[0];
+  //       setFormData({
+  //         ...response,
+  //         reservation_date: thisDate,
+  //       });
+  //     } catch (error) {
+  //       setError(error);
+  //       console.error(error);
+  //     }
+  //   }
+  //   loadEditReservation();
+  //   return () => {
+  //     abortController.abort();
+  //   };
+  // }, [reservation_id]);
 
-  const handleChange = ({ target }) => {
-    if (target.name === "mobile_number") {
-      const formatted = formatPhoneNumber(target.value);
-      setFormData({
-        ...formData,
-        [target.name]: formatted,
-      });
-    } else {
-      setFormData({
-        ...formData,
-        [target.name]: target.value,
-      });
-    }
-  };
+  // const handleChange = ({ target }) => {
+  //   if (target.name === "mobile_number") {
+  //     const formatted = formatPhoneNumber(target.value);
+  //     setFormData({
+  //       ...formData,
+  //       [target.name]: formatted,
+  //     });
+  //   } else {
+  //     setFormData({
+  //       ...formData,
+  //       [target.name]: target.value,
+  //     });
+  //   }
+  // };
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      const abortController = new AbortController();
-      formData.people = Number(formData.people);
+  // async function handleSubmit(e) {
+  //   e.preventDefault();
+  //   try {
+  //     const abortController = new AbortController();
+  //     formData.people = Number(formData.people);
 
-      const response = await editReservation(
-        { ...formData },
-        abortController.signal
-      );
-      history.push(`/dashboard?date=${formData.reservation_date}`);
-      return response;
-    } catch (error) {
-      setError(error);
-      console.error(error);
-    }
-  }
+  //     const response = await editReservation(
+  //       { ...formData },
+  //       abortController.signal
+  //     );
+  //     history.push(`/dashboard?date=${formData.reservation_date}`);
+  //     return response;
+  //   } catch (error) {
+  //     setError(error);
+  //     console.error(error);
+  //   }
+  // }
 
-  function formatPhoneNumber(value) {
-    if (!value) return value;
-    const phoneNumber = value.replace(/[^\d]/g, "");
-    const phoneNumberLength = phoneNumber.length;
-    if (phoneNumberLength < 4) return phoneNumber;
-    if (phoneNumberLength < 7) {
-      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
-    }
-    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
-      3,
-      6
-    )}-${phoneNumber.slice(6, 10)}`;
-  }
+  // function formatPhoneNumber(value) {
+  //   if (!value) return value;
+  //   const phoneNumber = value.replace(/[^\d]/g, "");
+  //   const phoneNumberLength = phoneNumber.length;
+  //   if (phoneNumberLength < 4) return phoneNumber;
+  //   if (phoneNumberLength < 7) {
+  //     return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+  //   }
+  //   return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+  //     3,
+  //     6
+  //   )}-${phoneNumber.slice(6, 10)}`;
+  // }
 
-  function handleCancel(e) {
-    e.preventDefault();
-    history.goBack();
-  }
+  // function handleCancel(e) {
+  //   e.preventDefault();
+  //   history.goBack();
+  // }
 
-  if (formData) {
-    if (formData.reservation_date) {
-      return (
-        <div className="container">
-          <form className="d-grid gap-2 mb-2" onSubmit={(e) => handleSubmit(e)}>
-            <h1>Edit Reservation</h1>
-            <ErrorAlert error={error} />
-            <div className="form-group">
-              <label>First Name:</label>
-              <input
-                id="first_name"
-                name="first_name"
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                value={formData.first_name}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Last Name:</label>
-              <input
-                id="last_name"
-                name="last_name"
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                value={formData.last_name}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Mobile Number:</label>
-              <input
-                id="mobile_number"
-                name="mobile_number"
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                maxLength="14"
-                size="14"
-                value={formData.mobile_number}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Date of Reservation:</label>
-              <input
-                id="reservation_date"
-                name="reservation_date"
-                className="form-control"
-                onChange={handleChange}
-                type="date"
-                value={formData.reservation_date}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Time of Reservation:</label>
-              <input
-                id="reservation_time"
-                name="reservation_time"
-                className="form-control"
-                onChange={handleChange}
-                type="time"
-                value={formData.reservation_time}
-                step="300"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>People:</label>
-              <input
-                id="people"
-                name="people"
-                className="form-control"
-                onChange={handleChange}
-                type="number"
-                value={formData.people}
-                required
-              />
-            </div>
-            <button className="btn btn-primary" type="submit">
-              Submit
-            </button>
-            <button
-              data-reservation-id-cancel={formData.reservation_id}
-              className="btn btn-secondary"
-              onClick={(e) => handleCancel(e)}
-            >
-              Cancel
-            </button>
-          </form>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
+  return (
+    // Form
+    <ReserveForm create={false} />
+  );
+
+  // if (formData) {
+  //   if (formData.reservation_date) {
+  //     return (
+  //       <div className="container">
+  //         <form className="d-grid gap-2 mb-2" onSubmit={(e) => handleSubmit(e)}>
+  //           <h1>Edit Reservation</h1>
+  //           <ErrorAlert error={error} />
+  //           <div className="form-group">
+  //             <label>First Name:</label>
+  //             <input
+  //               id="first_name"
+  //               name="first_name"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="text"
+  //               value={formData.first_name}
+  //               required
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label>Last Name:</label>
+  //             <input
+  //               id="last_name"
+  //               name="last_name"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="text"
+  //               value={formData.last_name}
+  //               required
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label>Mobile Number:</label>
+  //             <input
+  //               id="mobile_number"
+  //               name="mobile_number"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="text"
+  //               maxLength="14"
+  //               size="14"
+  //               value={formData.mobile_number}
+  //               required
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label>Date of Reservation:</label>
+  //             <input
+  //               id="reservation_date"
+  //               name="reservation_date"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="date"
+  //               value={formData.reservation_date}
+  //               required
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label>Time of Reservation:</label>
+  //             <input
+  //               id="reservation_time"
+  //               name="reservation_time"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="time"
+  //               value={formData.reservation_time}
+  //               step="300"
+  //               required
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label>People:</label>
+  //             <input
+  //               id="people"
+  //               name="people"
+  //               className="form-control"
+  //               onChange={handleChange}
+  //               type="number"
+  //               value={formData.people}
+  //               required
+  //             />
+  //           </div>
+  //           <button className="btn btn-primary" type="submit">
+  //             Submit
+  //           </button>
+  //           <button
+  //             data-reservation-id-cancel={formData.reservation_id}
+  //             className="btn btn-secondary"
+  //             onClick={(e) => handleCancel(e)}
+  //           >
+  //             Cancel
+  //           </button>
+  //         </form>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // } else {
+  //   return null;
+  // }
 }
 
 export default EditReservation;
